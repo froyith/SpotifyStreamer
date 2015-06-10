c116ca5daf074ec83fa1673678765124f0fcfe2e
Frank Smith <frank@flsee.com>
2015-06-10 19:41:53 -0400

    main ListView bound to temporary data adapter

--- app/src/main/java/com/froyith/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/com/froyith/spotifystreamer/MainActivityFragment.java
@@ -5,18 +5,77 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
-
+    private ArrayAdapter<String> mArtistAdapter;
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main,container,false);
+
+        String[] testData = {
+
+                "test1",
+                "test2",
+                "test3",
+                "test4",
+                "test5",
+                "test6",
+                "test7",
+                "test8",
+                "test9",
+                "test10",
+                "test11",
+                "test12",
+                "test13",
+                "test14",
+                "test15",
+                "test16",
+                "test17",
+                "test18",
+                "test19",
+                "test20",
+
+        };
+
+        List<String> testDataList = new ArrayList<String>(Arrays.asList(testData));
+
+         mArtistAdapter =
+                 new ArrayAdapter<String>(
+                        //context
+                        getActivity(),
+                        R.layout.artist_list_item_layout,
+                        R.id.list_item_artist_textview,
+                        testDataList);
+
+       //
+
+        ListView mlistView = (ListView) rootView.findViewById(
+
+                R.id.listview_artist);
+        mlistView.setAdapter(mArtistAdapter);
+        //
+
+
+        //inflater.inflate(R.layout.fragment_main, container, false)
+        return rootView;
+
+
+
+
+
+
     }
 }
